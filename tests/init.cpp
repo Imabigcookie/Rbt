879269bc1e128f32d3b4b13ac08d05d140b17c2b
Imabigcookie <sasher9851@gmail.com>
2017-06-15 10:45:44 +0300

    Create init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,18 +12,21 @@ SCENARIO("add")
 	a.Insert(5);
 	REQUIRE(!a.Empty());
 }
+
 SCENARIO("BST del non inserted element", "[delete]")
 {
 	RBT<int> tree = { 6 } ;
 	tree.Remove(3);
 	REQUIRE(!tree.Empty());
 }
+
 SCENARIO("BST del root with one element", "[delete]")
 {
 	RBT<int> tree = { 6 };
 	tree.Remove(6);
 	REQUIRE(tree.Empty());
 }
+
 SCENARIO("BST del root without child", "[delete]")
 {
 	RBT<int> tree = { 6, 4, 3 };
@@ -31,6 +34,7 @@ SCENARIO("BST del root without child", "[delete]")
 	RBT<int> tr ={ 4, 3 };
 	REQUIRE(tree ==tr);
 }
+
 SCENARIO("BST del root with two children", "[delete]")
 {
 	RBT<int> tree = { 6, 4, 3, 9, 8, 12, 10, 11 };
@@ -38,6 +42,7 @@ SCENARIO("BST del root with two children", "[delete]")
 	RBT<int>tr={ 8, 6,  10, 3, 9, 12, 11 };
 	REQUIRE(tree ==tr );
 }
+
 SCENARIO("BST del non root without children", "[delete]")
 {
 	RBT<int> tree = { 6, 4, 3, 9, 8, 12, 10, 11 };
@@ -45,6 +50,7 @@ SCENARIO("BST del non root without children", "[delete]")
 	RBT<int>tr= { 8,  4, 10, 9, 11, 12, 6 };
 	REQUIRE(tree == tr);
 }
+
 SCENARIO("BST del non root with one child", "[delete]")
 {
 	RBT<int> tree = { 6, 4, 3, 9, 8, 12, 10, 11 };
